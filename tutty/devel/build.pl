a7c0cdbbbb16473f26d8ecc7f79dcc00b0ea996f
Alexander Tokarev <dwalin@dwalin.ru>
2006-06-16 21:33:51 +0000

    switched repository to the new scheme. session api library is now separate.

--- tutty/devel/build.pl
+++ tutty/devel/build.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/perl
 
-open(BUILD, "/home/tutty/build.h") or die "cannot open build.h!";
+open(BUILD, "/home/devel/tutty/build.h") or die "cannot open build.h!";
 $line = <BUILD>;
 close(BUILD);
 
 ($define, $buildnumber, $build) = split(' ', $line);
 $build++;
 
-open(BUILD, ">/home/tutty/build.h");
+open(BUILD, ">/home/devel/tutty/build.h");
 print BUILD "#define BUILDNUMBER $build\n";
 close(BUILD);
 
-system("touch /home/tutty/build.h");
-system("touch /home/tutty/version.c");
+system("touch /home/devel/tutty/build.h");
+system("touch /home/devel/tutty/version.c");
